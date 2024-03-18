9135f3f54c6ae7b4c299fd530784c957fecb1d23
zhengpeijin95 <598359997@qq.com>
2024-03-18 15:26:30 +0800

    添加eslint约束

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended", // 需放到最后，才会使用此规则替换上面的规则
+  ],
+  // parserOptions: {
+  //     "ecmaVersion": "latest",
+  //     "sourceType": "module"
+  // },
+  rules: {
+    eqeqeq: ["warn", "smart"], // 必须使用 === 或 !==
+    // quotes: ["error", "double"], // 使用双引号 【prettier中处理了】
+    // semi: "error", // 禁止无分号 【prettier中处理了】
+    "@typescript-eslint/no-explicit-any": "off", // 允许隐式any类型
+    "@typescript-eslint/no-unused-vars": "off", // 允许声明但未使用的变量 0 = off, 1 = warn, 2 = error
+    "@typescript-eslint/prefer-for-of": "warn", // 如果索引仅用于访问要迭代的数组，则优先于for循环的for-of循环
+  },
+};
--- src/script/MainHp.ts
+++ src/script/MainHp.ts
@@ -1,12 +1,12 @@
-import {ui} from "../ui/layaMaxUI";
+import { ui } from "../ui/layaMaxUI";
 
 const HP_RES_ARY: string[] = [
-	"hp/img_hp1.png",
-	"hp/img_hp2.png",
-	"hp/img_hp3.png",
-	"hp/img_hp4.png",
-	"hp/img_hp5.png",
-	"hp/img_hp6.png"
+  "hp/img_hp1.png",
+  "hp/img_hp2.png",
+  "hp/img_hp3.png",
+  "hp/img_hp4.png",
+  "hp/img_hp5.png",
+  "hp/img_hp6.png",
 ];
 
 const HP_RES_END: string = "hp/img_hp7.png";
@@ -14,69 +14,69 @@ const HP_SINGLE: number = 1000;
 
 /**随机整数*/
 function getRandom(min: number, max: number): number {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 /**随机血量*/
 function createMaxHp(): number {
-	return getRandom(10, 15) * 1000;
+  return getRandom(10, 15) * 1000;
 }
 
 /**随机减血*/
 function createSubHp(): number {
-	return getRandom(0, 1000);
+  return getRandom(0, 1000);
 }
 
 /**
  * 血条
  */
 export default class MainHp extends ui.hp.MainHpUI {
-	private _maxHp: number;
-	private _leftHp: number;
-	private _radio: number = 0;
-	private _hpResIdx = 0;
+  private _maxHp: number;
+  private _leftHp: number;
+  private _radio: number = 0;
+  private _hpResIdx = 0;
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	public onAwake(): void {
-		super.onAwake();
-	}
+  public onAwake(): void {
+    super.onAwake();
+  }
 
-	public onEnable(): void {
-		super.onEnable();
-		this._maxHp = createMaxHp();
-		this._leftHp = this._maxHp;
-		this._radio = 1;
-		this.timerLoop(500, this, this.onUpdateHp);
-	}
+  public onEnable(): void {
+    super.onEnable();
+    this._maxHp = createMaxHp();
+    this._leftHp = this._maxHp;
+    this._radio = 1;
+    this.timerLoop(500, this, this.onUpdateHp);
+  }
 
-	private onUpdateHp(): void {
-		this._leftHp -= createSubHp();
-		if (this._leftHp < 0) {
-			this._leftHp = this._maxHp = createMaxHp();
-		}
-		const radio = this._leftHp / this._maxHp;
-		if (radio === this._radio) {
-			return;
-		}
-		this._radio = radio;
-		console.log(this._radio);
-		this.updateLabHp();
-		this.updateHpSkin();
-	}
+  private onUpdateHp(): void {
+    this._leftHp -= createSubHp();
+    if (this._leftHp < 0) {
+      this._leftHp = this._maxHp = createMaxHp();
+    }
+    const radio = this._leftHp / this._maxHp;
+    if (radio === this._radio) {
+      return;
+    }
+    this._radio = radio;
+    console.log(this._radio);
+    this.updateLabHp();
+    this.updateHpSkin();
+  }
 
-	private updateLabHp(): void {
-		const num = this._leftHp / this._maxHp * 100;
-		this.labHp.text = "x" + Math.ceil(this._leftHp / HP_SINGLE) + "    " + num.toFixed(2) + "%";
-	}
+  private updateLabHp(): void {
+    const num = (this._leftHp / this._maxHp) * 100;
+    this.labHp.text =
+      "x" + Math.ceil(this._leftHp / HP_SINGLE) + "    " + num.toFixed(2) + "%";
+  }
 
-	private updateHpSkin(): void {
-		if (this._hpResIdx >= HP_RES_ARY.length) {
-			this._hpResIdx = 0;
-		}
-		this.imgHp.skin = HP_RES_ARY[this._hpResIdx++];
-	}
-
+  private updateHpSkin(): void {
+    if (this._hpResIdx >= HP_RES_ARY.length) {
+      this._hpResIdx = 0;
+    }
+    this.imgHp.skin = HP_RES_ARY[this._hpResIdx++];
+  }
 }
