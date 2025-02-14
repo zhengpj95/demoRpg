1b86b0f4d91eaa9b2fca737e00e3f8d9c11c2742
zhengpj <598359997@qq.com>
2025-02-14 11:44:31 +0800

    添加通过load皮肤打开界面

--- src/modules/misc/cmd/OpenViewCmd.ts
+++ src/modules/misc/cmd/OpenViewCmd.ts
@@ -1,8 +1,13 @@
 import { BaseCommand } from "@base/mvc/BaseCommand";
 import { IOpenCloseData } from "@def/misc";
 import { facade } from "@base/mvc/Facade";
-import { LayerMgr } from "@base/LayerMgr";
+import { LayerIndex, LayerMgr } from "@base/LayerMgr";
 import { GEvent } from "@base/core/GEvent";
+import Handler = Laya.Handler;
+import ClassUtils = Laya.ClassUtils;
+import View = Laya.View;
+import Scene = Laya.Scene;
+import SceneUtils = Laya.SceneUtils;
 
 /**创建mdr唯一标识*/
 export function createMdrKey(data: IOpenCloseData): string {
@@ -32,5 +37,44 @@ export class OpenViewCmd extends BaseCommand {
     const mdrKey = createMdrKey(data);
     layer.doAddMdr(mdrCls, mdrKey);
     console.log(`OpenViewCmd 打开界面 m:${data.module},v:${data.view}`);
+  }
+}
+
+// 通过皮肤资源加载的方式处理
+export function showView(path: string): void {
+  const url = path.indexOf(".") > -1 ? path : path + ".scene";
+  const view = Laya.loader.getRes(url);
+  if (view) {
+    onLoaded(url, view);
+  } else {
+    Laya.loader.load(url, Handler.create(null, onLoaded, [url], true));
+  }
+}
+
+// todo 如果没有runtime的情况下，需要兼容通过url找到对应的mdr，执行运行mdr。通过 ClassUtils 注册和获取即可
+function onLoaded(url: string, obj: any): void {
+  console.log(`11111 ${url} `, obj);
+  const runtime = obj.props.runtime ? obj.props.runtime : obj.type;
+  const clas = ClassUtils.getClass(runtime);
+  const layer = LayerMgr.ins().getLayer(LayerIndex.WIN); //test
+  let scene: Scene | View | undefined;
+  if (obj.props.renderType === "instance") {
+    scene = clas.instance || (clas.instance = new clas());
+  } else {
+    scene = new clas();
+  }
+
+  if (scene && scene instanceof Scene && obj.props.runtime) {
+    // 配置了runtime的方式
+    layer.removeAll();
+    layer.addChild(scene);
+    scene.url = url;
+    scene.onOpened(null);
+  } else {
+    SceneUtils.createComp(obj, scene);
+    layer.removeAll();
+    layer.addChild(scene);
+    scene.url = url;
+    scene.onOpened(null); // todo 对应的mdr的方法没有执行到。因为这里scene是Scene或View的实例，不是mdr的实例
   }
 }
