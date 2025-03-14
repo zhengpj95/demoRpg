1f9554f150362c930919797d89e1f2108fef869e
zhengpj <598359997@qq.com>
2025-03-14 23:23:33 +0800

    CallBack重命名

--- src/base/CallBack.ts
+++ src/base/CallBack.ts
@@ -2,8 +2,8 @@
  * @author zpj
  * @date 2025/3/5
  */
-export class Callback<T extends any[] = [], R = void, C = any> {
-  private static _pool: Callback<any, any, any>[] = [];
+export class CallBack<T extends any[] = [], R = void, C = any> {
+  private static _pool: CallBack<any, any, any>[] = [];
   private static _idCounter = 0;
 
   private _id: number = 0;
@@ -16,19 +16,19 @@ export class Callback<T extends any[] = [], R = void, C = any> {
     context: C,
     func: (this: C, ...args: T) => R,
     once?: boolean,
-  ): Callback<T, R, C>;
+  ): CallBack<T, R, C>;
   public static alloc<T extends any[], R, C, A0>(
     context: C,
     func: (this: C, arg0: A0, ...args: T) => R,
     args: [A0],
     once?: boolean,
-  ): Callback<T, R, C>;
+  ): CallBack<T, R, C>;
   public static alloc<T extends any[], R, C, A0, A1>(
     context: C,
     func: (this: C, arg0: A0, arg1: A1, ...args: T) => R,
     args: [A0, A1],
     once?: boolean,
-  ): Callback<T, R, C>;
+  ): CallBack<T, R, C>;
 
   /**
    * 创建一个 Callback 实例，优先从对象池中获取
@@ -43,7 +43,7 @@ export class Callback<T extends any[] = [], R = void, C = any> {
     func: (this: C, ...args: T) => R,
     args?: T | boolean,
     once?: boolean,
-  ): Callback<T, R, C> {
+  ): CallBack<T, R, C> {
     console.log(11111, typeof func);
     if (typeof func !== "function") {
       throw new Error("CallBack func must be Function!");
@@ -54,9 +54,9 @@ export class Callback<T extends any[] = [], R = void, C = any> {
     }
     const instance =
       this._pool.length > 0
-        ? (this._pool.pop()! as Callback<T, R, C>)
-        : new Callback<T, R, C>();
-    instance._id = ++Callback._idCounter;
+        ? (this._pool.pop()! as CallBack<T, R, C>)
+        : new CallBack<T, R, C>();
+    instance._id = ++CallBack._idCounter;
     instance._context = context || null;
     instance._method = func;
     instance._args = <T>args;
@@ -97,7 +97,7 @@ export class Callback<T extends any[] = [], R = void, C = any> {
     this._args.length = 0;
     this._once = false;
     this._id = 0;
-    Callback._pool.push(this);
+    CallBack._pool.push(this);
   }
 
   /**
