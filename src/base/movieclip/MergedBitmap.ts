3e9299f3b0a3c0a098d7135db8980de2cd31ec12
zhengpj <598359997@qq.com>
2025-04-12 23:50:44 +0800

    MergedBitmap优化

--- src/base/movieclip/MergedBitmap.ts
+++ src/base/movieclip/MergedBitmap.ts
@@ -7,7 +7,7 @@ import Loader = Laya.Loader;
 import Texture = Laya.Texture;
 
 /**
- * 整合序列帧相关的png纹理集图片和atlas配置文件
+ * 整合序列帧相关的png纹理集图片和json配置文件
  * @date 2024/7/1
  */
 export class MergedBitmap implements IPoolObject {
@@ -17,6 +17,18 @@ export class MergedBitmap implements IPoolObject {
   private _callback: CallBack<[MergedBitmap]>;
   private _texture: Texture;
   private _textureList: Texture[];
+
+  public get frames(): IFrameData[] | undefined {
+    return this._frames;
+  }
+
+  public get texture(): Texture | undefined {
+    return this._texture;
+  }
+
+  public get loadComplete(): boolean {
+    return !!this.frames && !!this.texture;
+  }
 
   public static onLoad(url: string, callback: CallBack<[MergedBitmap]>): void {
     const bitmap = PoolMgr.alloc(MergedBitmap);
@@ -69,28 +81,23 @@ export class MergedBitmap implements IPoolObject {
 
   private onLoadPng(data: Texture): void {
     this._texture = data;
-    console.log(`11111 png: `, data, data instanceof Texture);
     this.onLoadComplete();
   }
 
   private onLoadJson(data: ITextureAtlas): void {
     this._atlas = data;
-    this._frames = data.frames;
-    console.log(`11111 json: `, data);
+    this._frames = data ? data.frames : <any>undefined;
+    this.onLoadComplete();
   }
 
   private onLoadComplete(): void {
-    if (this._callback && this.isLoadComplete) {
+    if (this._callback && this.loadComplete) {
       this._callback.exec(this);
     }
   }
 
-  private isLoadComplete(): boolean {
-    return !!this._frames && !!this._atlas;
-  }
-
   public getTextureList(): Texture[] {
-    if (!this.isLoadComplete()) return [];
+    if (!this.loadComplete) return [];
     if (this._textureList) return this._textureList;
 
     this._textureList = [];
