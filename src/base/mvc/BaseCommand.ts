5127f317e2a94cb28dca2bee2613fb426412b34a
zhengpj <598359997@qq.com>
2025-04-26 21:22:21 +0800

    mvc模式优化

--- src/base/mvc/BaseCommand.ts
+++ src/base/mvc/BaseCommand.ts
@@ -1,6 +1,12 @@
+import { emitter } from "@base/MessageMgr";
+
 /**
  * @date 2024.4.13
  */
 export abstract class BaseCommand {
   public abstract exec(args: any): void;
+
+  public emit(event: string, args?: any): void {
+    emitter.emit(event, args);
+  }
 }
